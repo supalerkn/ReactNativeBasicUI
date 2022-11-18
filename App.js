8f3a733626060edc4697d2e0b9bd38d884bf08b0
Supalerk <supalerk@Supalerks-MacBook-Pro.local>
2022-11-18 11:02:36 +0700

    edit App.js

--- App.js
+++ App.js
@@ -76,8 +76,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            Supalerk first edit file.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
